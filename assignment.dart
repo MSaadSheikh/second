e5142e629631f33c668b0217d3aa15fb78b294bd
MSaadSheikh <95358942+MSaadSheikh@users.noreply.github.com>
2022-02-07 04:06:09 -0800

    Add files via upload

---
+++ assignment.dart
@@ -0,0 +1,140 @@
+import 'dart:io';
+void main() {
+
+//   Program1
+  //stdout.write("Add Visitor Name: ");
+  //var visi=stdin.readLineSync();
+
+//stdout.write("Product Title: ");
+  //var product=stdin.readLineSync();
+
+  //stdout.write("Quantity: ");
+  //var quantity=stdin.readLineSync();
+  
+  //var quantity=int.parse(quantity ??"");
+  //print("Wellcome Mr : ${visitor} your Order ${quantity} ${product} on SS Production");
+
+//  Program 2
+
+  //  stdout.write("Enter no: 1 ");
+  // var data= stdin.readLineSync();
+  // var s1 = int.parse(data ?? "");
+  
+  // stdout.write("Enter no: 2 ");
+  // var data2= stdin.readLineSync();
+  // var s2 = int.parse(data2 ?? "");
+
+//   Program3
+
+  // int x=s1+s2;
+  // int y=s1-s2;
+  // int z=s1*s2;
+  // double c=s1/s2;
+  // print(x);
+  // print(y);
+  // print(z);
+  // print(c);
+
+//   Program4
+
+  // int cost=500;
+  // stdout.write("Ticket price is : 500 \n ");
+  // stdout.write("Ticket Quan : ");
+  // int? Tic=int.parse(stdin.readLineSync()!);
+  // var t= Tic*cost;
+  // print("Total Amount of $t");
+
+//   Program5
+
+  // stdout.write("Add the Current Temprature : ");
+  // int? Temperature=int.parse(stdin.readLineSync()!);
+  // stdout.write("Recent Temprature is :$Temp C \n");
+  // var f=(Temp*10/4)+39;
+  // print("The Value of Formula is : $f");
+
+//   Program6
+
+  // stdout.write("Shopping Cart \n");
+  // stdout.write("first item price is : ");
+  // int? price=int.parse(stdin.readLineSync()!);
+  // stdout.write("first item Quantity is : ");
+  // int? quantity=int.parse(stdin.readLineSync()!);
+
+  // stdout.write("second item price is : ");
+  // int? price2=int.parse(stdin.readLineSync()!);
+  // stdout.write("second item Quantity is : ");
+  // int? quatity2=int.parse(stdin.readLineSync()!);
+  // var s=price * quantity + price2 * quantity2;
+  // print(s);
+
+  // Program7
+
+  // stdout.write("Total marks is : ");
+  // int? totalmarks=int.parse(stdin.readLineSync()!);
+  // stdout.write("obtained marks is :");
+  // int? obtainmarks=int.parse(stdin.readLineSync()!);
+  // var a=obtainmarks / totalmarks * 100;
+  // print(a);
+
+//   Program8
+
+
+//   Program9
+
+  //  stdout.write("Enter Hours : ");
+  //  int? Hour = int.parse(stdin.readLineSync()!);
+  //  stdout.write("Enter rate per Hour : ");
+  //  int? rate = int.parse(stdin.readLineSync()!);
+  //  int week = Hour * rate ;
+  //  stdout.write("Your weekly earning is :$week ");
+
+// Program10
+
+// String data= "Dart Course";
+// String d=data[0].toUpperCase()+data.substring(1).toLowerCase();
+// String d=data.toUpperCase();
+// String d=data.toLowerCase();
+// print(d);
+
+// stdout.write("enter number:");
+// int digit=int.parse(stdin.readLineSync()!);
+// var total= (dig+5)*10/2;
+// print(total);
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+// var r1= --a;
+// var r2= --a - --b;
+// var r3= --a - --b + ++b;
+// var r4= --a - --b + ++b + b--;
+// print(result);
+// print(r1);
+// print(r2);
+// print(r3);
+// print(r4);
+
+//  String d="SMIT";
+//  String d2="SMIT";
+//  print(d.compareTo(d2));
+
+// String data="DELL";
+// int d=data.length;
+// print(d); 
+
+// String data = 'AbBTE';
+// int d = data.codeUnitAt(1);
+// print(d);
+
+
+
+// String data= "ZEBTECH";
+// var var2=data.split("");
+// print(var2);
+
+// stdout.write("Write Something");
+// String get = stdin.readLineSync()!;
+// var check = get.substring(4,7);
+// print("$check");
+
+// }
